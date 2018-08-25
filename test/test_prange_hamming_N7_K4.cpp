7ca19c745821fadfd892ecd65b43c94c7eb9fa28
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2018-08-25 18:57:59 +0200

    Updated Hamming and Goppa test with input from files
    Previously hardcoded, Hamming and Goppa tests now exploit the file input functions, with H/G/x/e data taken from files in the /test/dat subfolder.
    The same approach will be crucial in testing of much larger codes.

--- test/test_prange_hamming_N7_K4.cpp
+++ test/test_prange_hamming_N7_K4.cpp
@@ -1,65 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <bitset>
 #include "prange_isd.h"
-
-using namespace std;
+#include "prange_file.h"
 
 int main() {
     vector<bitset<N> > h;
-    int w = 1;
-    bitset<N> e;
-
-    // Hamming (7,4)
-    // bitsets are rows of the H matrix
-    bitset<N> b0 ("1010101");
-    bitset<N> b1 ("1100110");
-    bitset<N> b2 ("0001111");
-
-    // N-K bitsets (N bits long)
-    h.push_back(b0);
-    h.push_back(b1);
-    h.push_back(b2);
-
-    // Hamming (7,4) error-free words
-    bitset<N> y1 ("1010101");
-    bitset<N> y2 ("1110000");
-    bitset<N> y3 ("0100101");
-    bitset<N> y4 ("1101001");
-    bitset<N> y5 ("1001100");
-
-    vector<bitset<N> > yvec;
-    yvec.push_back(y1);
-    yvec.push_back(y2);
-    yvec.push_back(y3);
-    yvec.push_back(y4);
-    yvec.push_back(y5);
-
-    // Weight 1 errors
-    bitset<N> e1 ("1000000");
-    bitset<N> e2 ("0100000");
-    bitset<N> e3 ("0010000");
-    bitset<N> e4 ("0001000");
-    bitset<N> e5 ("0000100");
-    bitset<N> e6 ("0000010");
-    bitset<N> e7 ("0000001");
-
+    vector<bitset<K> > xvec;
+    vector<bitset<K> > g;
     vector<bitset<N> > evec;
-    evec.push_back(e1);
-    evec.push_back(e2);
-    evec.push_back(e3);
-    evec.push_back(e4);
-    evec.push_back(e5);
-    evec.push_back(e6);
-    evec.push_back(e7);
+    bitset<N> c;
+    bitset<N> e;
+    bitset<K> tmp;
+    int w;
 
+    string h_file = "dat/hamming_N7_K4_h.dat";
+    string x_file = "dat/hamming_N7_K4_x.dat";
+    string g_file = "dat/hamming_N7_K4_g.dat";
+    string e_file = "dat/hamming_N7_K4_e.dat";
 
-    cout << "Test ID\tOutcome\tExpected\tResult" << endl;
+    // Hamming(7, 4)
+    read_bitsetN_vectors_from_file (h_file, h);
+    read_bitsetK_vectors_from_file (x_file, xvec);
+    read_bitsetK_vectors_from_file (g_file, g);
+    read_bitsetN_vectors_from_file(e_file, evec);
 
-    for (int i=0; i<5; i++) {
+    cout << "TestID\tOutcome\tExpected\tResult" << endl;
+
+    for (int i=0; i<xvec.size(); i++) {
+        // c = G * x
         for (int j=0; j<N; j++) {
-            isd (h, yvec[i]^evec[j], 1, e);
-            cout << "1T" << i << "." << j << "\t" << ((e == evec[j]) ? "OK" : "NO") << "\t" << e << "\t\t" << evec[j] << endl;
+            tmp = g[j] & xvec[i];
+            c[j] = (tmp.count() % 2);
+        }
+
+        for (int j=0; j<evec.size(); j++) {
+            w = evec[j].count();
+            isd (h, c^evec[j], w, e);
+            cout << w << "T" << i << "." << j << "\t" << ((e == evec[j]) ? "OK" : "NO ") << "\t" << e << "\t\t" << evec[j] << endl;
         }
     }
 
